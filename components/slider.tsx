3a024f45181aea0a3c054061e06d07bee4ce8516
Toowaa <brahanax@gmail.com>
2025-01-29 23:59:27 -0500

    Add SliderComponent with background image and styling

---
+++ components/slider.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+export default function SliderComponent() {
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <div className="w-full h-1/2 flex items-center justify-center">
+                <img src="/images/ServiceBackground2.webp" alt="background" className="w-full h-full object-cover" />   
+         </div>
+         </div>       
+      )
+}
