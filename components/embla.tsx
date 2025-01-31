58f3012d6527fee08cfb2d222bc12e90b86a64cb
Toowaa <brahanax@gmail.com>
2025-01-31 18:57:07 -0500

    Add new image assets for couple therapy section and update icon paths in Embla component

--- components/embla.tsx
+++ components/embla.tsx
@@ -290,27 +290,27 @@ const carrouseldata = [
       {
         id: 1,
         text: "Sexualidad",
-        iconImage: "/Services/teen/emociones.webp",
+        iconImage: "/Services/couple/sexualidad.webp",
       },
       {
         id: 2,
         text: "Problemas de comunicación",
-        iconImage: "/Services/teen/estres.webp",
+        iconImage: "/Services/couple/problemascomuni.webp",
       },
       {
         id: 3,
         text: "Celos",
-        iconImage: "/Services/teen/autoestima.webp",
+        iconImage: "/Services/couple/celos.webp",
       },
       {
         id: 4,
         text: "Familia",
-        iconImage: "/Services/teen/sociales.webp",
+        iconImage: "/Services/couple/familia.webp",
       },
       {
         id: 5,
         text: "Dependencia emocional",
-        iconImage: "/Services/teen/vocacional.webp",
+        iconImage: "/Services/couple/dependencia.webp",
       },
     ],
     tittlecards: "Beneficios de la terapia de parejas",
@@ -318,27 +318,27 @@ const carrouseldata = [
       {
         id: 1,
         text: "Aprenderán a comunicarse y expresar sus emociones de manera clara y efectiva, fortaleciendo el entendimiento mutuo.",
-        icon: "/Services/teen/therapy/frustraciones.webp",
+        icon: "/Services/couple/therapy/comunicarse.webp",
       },
       {
         id: 2,
         text: "Incrementará la satisfacción y el bienestar, no solo en la relación, sino también a nivel personal.",
-        icon: "/Services/teen/therapy/equilibrio.webp",
+        icon: "/Services/couple/therapy/satisfaccion.webp",
       },
       {
         id: 3,
         text: "Desarrollarán una empatía más profunda para comprender y atender mejor las necesidades de su pareja.",
-        icon: "/Services/teen/therapy/comunicacion.webp",
+        icon: "/Services/couple/therapy/empatia.webp",
       },
       {
         id: 4,
         text: "Mejorarán su empatía, facilitando el respeto hacia sus diferencias y promoviendo la armonía.",
-        icon: "/Services/teen/therapy/seguridad.webp",
+        icon: "/Services/couple/therapy/armonia.webp",
       },
       {
         id: 5,
         text: "Construirán su mejor versión individual para crecer y avanzar juntos como pareja.",
-        icon: "/Services/teen/therapy/construir.webp",
+        icon: "/Services/couple/therapy/avanza.webp",
       },
     ],
     textfooter:
